4dd74bdbd274b56a618283199b596c467bfda3c0
Bruna Mello <brunacrespomello@gmail.com>
2021-05-04 05:06:46 +0100

    Static charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,5 +1,7 @@
+import Chart from "react-apexcharts";
 
 const DataTable = () => {
+
     return (
         <div className="table-responsive">
             <table className="table table-striped table-sm">
@@ -20,37 +22,6 @@ const DataTable = () => {
                     <td>25</td>
                     <td>15017.00</td>
                 </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
-                <tr>
-                    <td>22/04/2021</td>
-                    <td>Barry Allen</td>
-                    <td>34</td>
-                    <td>25</td>
-                    <td>15017.00</td>
-                </tr>
-
-
-
                 </tbody>
             </table>
         </div>
