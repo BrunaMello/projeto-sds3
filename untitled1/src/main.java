bd4543d0194e7a2f915c99039ff2632654c271c1
Bruna Mello <brunacrespomello@gmail.com>
2021-05-05 21:17:45 +0100

    Security Config

---
+++ untitled1/src/main.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public static void main(String[] args) {
+        Object obj = new Object();
+        String str = (String) obj;
+
+        System.out.println(str);
+    }
+}
